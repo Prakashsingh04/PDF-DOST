2ce4b452a1c0da118a57445c8a83a2d5254f8ded
Prakash <prakash82993@gmail.com>
2025-12-01 18:12:38 +0530

    connected frontend and backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,25 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
 
 // Upload PDF
 export const uploadPDF = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
+
   const res = await axios.post(`${API_BASE}/upload`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
+
   return res.data;
 };
 
-// Extract chunks
 export const extractChunks = async (filename) => {
   const res = await axios.post(`${API_BASE}/extract`, { filename });
   return res.data;
 };
 
-// Ask a question
 export const askQuestion = async (filename, question) => {
   const res = await axios.post(`${API_BASE}/ask`, { filename, question });
   return res.data;
